cfd04ce3f5977f6e0585709b55155453834155a1
Brian Shao <brian@jspr.sh>
2025-03-05 17:38:24 -0800

    chore: separate test package

--- id_test.go
+++ id_test.go
@@ -1,6 +1,10 @@
-package souvenir
+package souvenir_test
 
-import "testing"
+import (
+	. "jasperlabs.net/pkg/souvenir"
+
+	"testing"
+)
 
 type TestType struct{}
 
@@ -19,15 +23,15 @@ func TestBytes(t *testing.T) {
 	id, _ := ParseID[TestType]("u_3456789abc0000123456789abc")
 	bytes := [16]byte{0x64, 0x29, 0x8e, 0x84, 0xa9, 0x6c, 0x00, 0x00, 0x00, 0x88, 0x64, 0x29, 0x8e, 0x84,
 		0xa9, 0x6c}
-	if id.data != bytes {
-		t.Errorf(`Expected %v but got %v`, bytes, id.data)
+	if id.Bytes() != bytes {
+		t.Errorf(`Expected %v but got %v`, bytes, id.Bytes())
 	}
 
 	id2, _ := ParseID[TestType]("u_7zzzzzzzzzzzzzzzzzzzzzzzzz")
 	bytes2 := [16]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
 		0xff, 0xff}
-	if id2.data != bytes2 {
-		t.Errorf(`Expected %v but got %v`, bytes2, id2.data)
+	if id2.Bytes() != bytes2 {
+		t.Errorf(`Expected %v but got %v`, bytes2, id2.Bytes())
 	}
 
 }
@@ -42,15 +46,15 @@ func TestRoundTrip(t *testing.T) {
 			t.Errorf(`ParseID error: %q`, err)
 		}
 
-		if id.data != id2.data {
-			t.Errorf(`Round trip mismatch: %q %q`, id.data, id2.data)
+		if id.Bytes() != id2.Bytes() {
+			t.Errorf(`Round trip mismatch: %q %q`, id.Bytes(), id2.Bytes())
 		}
 
 		uuid := id.UUID()
 		id3 := ParseUUID[TestType](uuid)
 
-		if id3.data != id.data {
-			t.Errorf(`Round trip uuid mismatch: %q %q`, id.data, id2.data)
+		if id3.Bytes() != id.Bytes() {
+			t.Errorf(`Round trip uuid mismatch: %q %q`, id.Bytes(), id2.Bytes())
 		}
 	}
 }
